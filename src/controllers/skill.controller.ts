62835f157ea577e3976c832d672d58ad5fb0546e
Maxime <maxime.moellic@gmail.com>
2025-06-13 10:55:07 +0200

    feat: create skillcontroller

---
+++ src/controllers/skill.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { Skill } from '../models/associations';
+import { Sequelize } from 'sequelize';
+
+interface SkillController {
+  getAllSkills(req: Request, res: Response) => Promise<Response>;
+}
+
+
+const skillController: SkillController = {
+
+    getAllSkills: async (req: Request, res: Response) => {
+        try {
+            const skills = await Skill.findAll({
+                attributes: ['id', 'name'],
+                order: [['name', 'ASC']]
+            });
+            return res.status(200).json(skills);
+        } catch (error) {
+            console.error('Error fetching skills:', error);
+            return res.status(500).json({ message: 'Internal server error' });
+        }
+    }
+}
+
+    export default skillController;
